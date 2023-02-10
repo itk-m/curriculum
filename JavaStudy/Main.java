b75e086a4bc12f4de9b23b313e81232fd0aa6b35
itk-m <itsuki.0904@outlook.com>
2023-02-10 22:00:10 +0900

    Update Main.java

--- JavaStudy/Main.java
+++ JavaStudy/Main.java
@@ -1,7 +1,10 @@
 
 public class Main  {
   public static void main(String[] args){
-     int hogeInt = 50;
+     int hogeInt = 50; 
      System.out.println("int型 =  整数 = " + hogeInt);
+
+     String hogeString = "テスト";
+     System.out.print(hogeString);
   }
 }
