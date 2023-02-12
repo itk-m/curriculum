61f09d9b53aacb1656bd4deabddb32a955a40e37
itk-m <itsuki.0904@outlook.com>
2023-02-12 11:03:54 +0900

    Update Main.java

--- 3-1/Main.java
+++ 3-1/Main.java
@@ -17,22 +17,6 @@ public class Main{
         for(int i = 0; i < data.length; i++){
             System.out.print(data[i] + " ");
         }
-        System.out.println();
-        for (int i = 0; i < 12375; i++) {
-            for (int j = data.length; j > i; j--) {
-                /*
-                * 問3
-                * 以下、配列の添字を入れてソートを完成させなさい
-                */
-                if(data[j] > data[i]){
-                  int box = data[i];
-                  data[i] = data[j];
-                  data[i] = box;
-                }
-            }
-        }
-        for(int i = 0; i < data.length; i++){
-            System.out.print(data[i] + " ");
-        }
+       
     }
 }
