4ae857f5a6bebed3d24d530583fc9a1515e1204b
itk-m <itsuki.0904@outlook.com>
2023-02-17 21:39:27 +0900

    Update practice.java

--- JavaStudy/practice.java
+++ JavaStudy/practice.java
@@ -1,10 +1,10 @@
 public class practice {
   public static void main(String[] args){
     // int配列の宣言と初期化 
-int[] arr = {5, 8, 2, 6}; 
+int[] arr = {5, 8, 2, 6, 8 , 9 , 10 ,8 , 10 ,10, 10}; 
 
 // 配列arrの中身を全て表示する 
-for (int i = 0; i < arr.length; i++) { 
+for (int i = 0; i < 12; i++) { 
 System.out.println("arr[" + i + "] … " + arr[i]); 
 }
   }
