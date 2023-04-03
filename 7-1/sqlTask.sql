332b0398971b0477d35b4464206426666c1db5c5
itk-m <itsuki.0904@outlook.com>
2023-04-03 18:45:39 +0900

    Update sqlTask.sql

--- 7-1/sqlTask.sql
+++ 7-1/sqlTask.sql
@@ -11,3 +11,9 @@ SELECT AVG(price) FROM goods_table
 ---------問⑤-----
 SELECT store_code FROM stock_table
 WHERE quantity > (SELECT AVG(quantity) FROM stock_table WHERE store_code = 'EA01')
+
+---------問⑥-----
+INSERT INTO store_table
+(goods_code , goods_name , price , update_day)
+VALUES
+("M001" , "マフラー" , 4500 , "2023-4-3");
