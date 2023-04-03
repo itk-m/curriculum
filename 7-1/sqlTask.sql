21f2621d3714a521b094c3ba1358bb3b6b1c6b04
itk-m <itsuki.0904@outlook.com>
2023-04-03 18:36:16 +0900

    Update sqlTask.sql

--- 7-1/sqlTask.sql
+++ 7-1/sqlTask.sql
@@ -8,3 +8,6 @@ SELECT * FROM store_table ORDER BY store_nameabc ASC
 ---------問④-----
 SELECT AVG(price) FROM goods_table
 
+---------問⑤-----
+SELECT store_code FROM stock_table
+WHERE quantity > (SELECT AVG(quantity) FROM stock_table WHERE store_code = 'EA01')
